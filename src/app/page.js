b5ed52d3a594ee44205864cdbf081464e8b41149
Juliana-CH <106660280+Juliana-CH@users.noreply.github.com>
2023-06-20 23:10:19 +0200

    - Finishing styles; - Adding css variables.

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,65 @@
+'use client'
+import { createContext } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
+
+const Context = createContext()
 
 export default function Home() {
   return (
 
-    <main className={styles}>
+    <body className={styles.body}>
 
       <div className={styles}>
 
         <header className={styles.header}>
           <div className={styles.headerImg}>
-          <Image src='/Image/bg1.jpg' alt='' width={500} height={300}/>
-          <Image src='/Image/bg2.jpg' alt='' width={500} height={300}/>
+          {/*<Image src='/Image/background-amfi.png' alt='' width={500} height={130}/>
+          <Image src='/Image/background-amfi.png' alt='' width={500} height={130}/>*/}
           </div>
           <h1 className={styles.headerTitle}>ToDo Amfi</h1>
         </header>
 
-        <section>
+        <section className={styles.toDoList}>
 
-          <div>
-            <h2>ToDo</h2>
+          <div className={styles.toDoListTitle}>
+            <h2>
+              🗂 ToDo
+            </h2>
           </div>
 
-          <div>
-            <h3>Tarefa 1</h3>
-            <h3>Tarefa 2</h3>
-            <h3>Tarefa 3</h3>
-            <h3>Tarefa 4</h3>
-            <h3>Tarefa 5</h3>
+          <div className={styles.toDoListItems}>
+
+            <h3 className={styles.toDoListItem}>
+              <input className={`${styles.toDoListRadio} ${styles['toDoListRadioChecked']}`} type='radio' />Tarefa 1
+            </h3>
+
+            <h3 className={styles.toDoListItem}>
+              <input className={`${styles.toDoListRadio} ${styles['toDoListRadioChecked']}`} type='radio'/>Tarefa 2
+            </h3>
+
+            <h3 className={styles.toDoListItem}>
+              <input className={`${styles.toDoListRadio} ${styles['toDoListRadioChecked']}`} type='radio'/>Tarefa 3
+            </h3> 
+
+            <h3 className={styles.toDoListItem}>
+              <input className={`${styles.toDoListRadio} ${styles['toDoListRadioChecked']}`} type='radio'/>Tarefa 4
+            </h3> 
+
+            <h3 className={styles.toDoListItem}>
+              <input className={`${styles.toDoListRadio} ${styles['toDoListRadioChecked']}`} type='radio'/>Tarefa 5
+            </h3>    
+          
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <button className={styles.addTaskButton}> + Adicionar Tarefa </button>
           </div>
 
         </section>
 
       </div>
 
-    </main>
+    </body>
   )
 }
